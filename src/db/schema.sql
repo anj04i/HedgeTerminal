06059f318956ce95a25084a9ac23860e0f72b880
0xahzam <ahzam.main@gmail.com>
2025-02-24 10:53:43 +0530

    chore: add indexes

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE FILINGS (
     total_value NUMERIC NOT NULL,
     PRIMARY KEY (cik, accession_number)
 );
+
+CREATE INDEX idx_filings_date ON FILINGS(filing_date);
+
+CREATE INDEX idx_filings_name ON FILINGS(name);
+
+CREATE INDEX idx_filings_total_value ON FILINGS(total_value);
+
+CREATE INDEX idx_filings_cik_date ON FILINGS(cik, filing_date);
