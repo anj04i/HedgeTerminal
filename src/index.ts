b92c920b8c8ba7f3c7fa86f146783db2b007be86
0xahzam <ahzam.main@gmail.com>
2025-03-17 22:05:26 +0530

    chore: add test

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { EdgarManager } from './lib/manager';
 
 async function test() {
   const edgar = new EdgarManager(1067983);
-  // const details = await edgar.getSecFilingsDetails();
-  // if (!details.success) return [];
+  const details = await edgar.get13FHistory();
+  return details;
 
-  const lastBuys = await edgar.getLatestBuys();
-  if (!lastBuys.success) return [];
-  return lastBuys;
+  // const lastBuys = await edgar.getLatestBuys();
+  // if (!lastBuys.success) return [];
+  // return lastBuys;
 }
 
 console.log(await test());
